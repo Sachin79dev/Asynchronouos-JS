ae393bb10cc25333bc5ebd53526e6f3a1f2a752d
Sachin Singh <90830959+Sachin79dev@users.noreply.github.com>
2026-07-15 18:29:31 +0530

    Add greet and welcome functions with callback example
    Add greet and welcome functions with callback example

--- app.js
+++ app.js
@@ -7,3 +7,20 @@ setTimeout(() => {
 }, 1000);
 
 console.log("End");
+
+
+
+
+
+// Q2. Simple Callback
+
+function greet(name) {
+  return `Hello ${name}`;
+}
+
+function welcome(callback) {
+  let user = "Sachin";
+  return callback(user);
+}
+
+console.log(welcome(greet)); // Hello Sachin
